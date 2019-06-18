151efa0fa853988f5fc5c40ccc33e0b01cb64f50
Derek Chen <derekchen14@gmail.com>
2019-06-17 23:14:41 -0400

    Add background able to autoplay

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
 #Debug logger
-import logging 
+import logging
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
@@ -18,44 +18,43 @@ ch.setFormatter(formatter)
 root.addHandler(ch)
 
 
-def return_dict():
+def get_data():
   #Dictionary to store music file information
-  dict_here = [
+  data_list = [
     {'id': 0, 'name': 'All of Me', 'link': 'songs/all_of_me.mp3', 'artist': 'John Legend', 'cover': 'Lindsey Stirling'},
-    {'id': 1, 'name': 'Brown Eyes', 'link': 'songs/brown_eyes.mp3', 'artist': 'Destiny\'s Child'},
-    {'id': 2, 'name': 'Burn', 'link': 'songs/burn.mp3', 'artist': 'Usher'},
-    {'id': 3, 'name': 'Home', 'link': 'songs/home.mp3', 'artist': 'Michael Bublé'},
+    {'id': 1, 'name': 'Brown Eyes', 'link': 'songs/brown_eyes.mp3', 'artist': 'Destiny\'s Child', 'cover': 'n/a'},
+    {'id': 2, 'name': 'Burn', 'link': 'songs/burn.mp3', 'artist': 'Usher', 'cover': 'n/a'},
+    {'id': 3, 'name': 'Home', 'link': 'songs/home.mp3', 'artist': 'Michael Bublé', 'cover': 'n/a'},
     {'id': 4, 'name': 'How Will I Know','link': 'songs/how_will_i_know.mp3', 'artist': 'Whitney Houston', 'cover': 'Sam Smith'},
-    {'id': 5, 'name': 'How You Gonna Act Like That','link': 'songs/how_you_gonna_act.mp3', 'artist': 'Tyrese'},
-    {'id': 6, 'name': 'I Wanna Know', 'link': 'songs/i_wanna_know.mp3', 'artist': 'Joe'},
-    {'id': 7, 'name': 'Make You Feel My Love', 'link': 'songs/make_you_feel_my_love.mp3', 'artist': 'Adele'},
-    {'id': 8, 'name': 'Photograph', 'link': 'songs/photograph.mp3', 'artist': 'Ed Sheeran', 'cover': 'Brooklyn'},
+    {'id': 5, 'name': 'How You Gonna Act Like That','link': 'songs/how_you_gonna_act.mp3', 'artist': 'Tyrese', 'cover': 'n/a'},
+    {'id': 6, 'name': 'I Wanna Know', 'link': 'songs/i_wanna_know.mp3', 'artist': 'Joe', 'cover': 'n/a'},
+    {'id': 7, 'name': 'Make You Feel My Love', 'link': 'songs/make_you_feel_my_love.mp3', 'artist': 'Adele', 'cover': 'n/a'},
+    {'id': 8, 'name': 'Photograph', 'link': 'songs/photograph.mp3', 'artist': 'Ed Sheeran', 'cover': 'Brooklyn Duo'},
     {'id': 9, 'name': 'Reason', 'link': 'songs/reason.mp3', 'artist': 'Calum Scott', 'cover': 'Leona Lewis'},
-    {'id': 10, 'name': 'Too Good at Goodbyes', 'link': 'songs/too_good.mp3', 'artist': 'Sam Smith'}
+    {'id': 10, 'name': 'Too Good at Goodbyes', 'link': 'songs/too_good.mp3', 'artist': 'Sam Smith', 'cover': 'n/a'}
     ]
-  return dict_here
+  return data_list
 
 # Initialize Flask.
 app = Flask(__name__)
 
 @app.route('/')
-def show_entries():
-  stream_entries = return_dict()
-  return render_template('index.html', entries=stream_entries)
+def load_homepage():
+  stream_entries = get_data()
+  return render_template('index.html', entries=stream_entries, na='n/a')
 
 #Route to render GUI
 @app.route('/play')
-def load_homepage():
+def show_entries():
   general_Data = { 'title': 'Play'}
-  # print(return_dict())
-  stream_entries = return_dict()
+  stream_entries = get_data()
   return render_template('play.html', entries=stream_entries, **general_Data)
 
 #Route to stream music
 @app.route('/<int:stream_id>')
 def streammp3(stream_id):
   def generate():
-    data = return_dict()
+    data = get_data()
     count = 1
     for item in data:
       if item['id'] == stream_id:
--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,11 +1,31 @@
 var song_count = 0;
+var music = document.getElementById("player-0");
 
 $( document ).ready(function() {
   $("div.well.id-" + song_count.toString()).show();
 });
 
-$("h1").click( function(){
+$("img.forward").click( function(){
+  $("div.well.id-" + song_count.toString()).hide();
+  music.pause();
+  song_count = (song_count+1) % 11;
+  $("div.well.id-" + song_count.toString()).show();
+});
+
+$("img.backward").click( function(){
+  $("div.well.id-" + song_count.toString()).hide();
+  music.pause();
+  song_count = (song_count == 0) ? 10 : (song_count-1) % 11;
+  $("div.well.id-" + song_count.toString()).show();
+});
+
+
+$(".radio").bind("ended", function(){
   $("div.well.id-" + song_count.toString()).hide();
   song_count = (song_count+1) % 11;
   $("div.well.id-" + song_count.toString()).show();
+
+  music = document.getElementById("player-" + song_count.toString());
+  music.load();
+  music.play();
 });
