54534a5047cb9eee5ec03050785180363c104a67
Derek Chen <derekchen14@gmail.com>
2019-06-19 23:19:24 -0400

    Move all songs into the static folder

--- app.py
+++ app.py
@@ -21,18 +21,24 @@ root.addHandler(ch)
 def get_data():
   #Dictionary to store music file information
   data_list = [
-    {'id': 0, 'name': 'All of Me', 'link': 'songs/all_of_me.mp3', 'artist': 'John Legend', 'cover': 'Lindsey Stirling'},
-    {'id': 1, 'name': 'Brown Eyes', 'link': 'songs/brown_eyes.mp3', 'artist': 'Destiny\'s Child', 'cover': 'n/a'},
-    {'id': 2, 'name': 'Burn', 'link': 'songs/burn.mp3', 'artist': 'Usher', 'cover': 'n/a'},
-    {'id': 3, 'name': 'Home', 'link': 'songs/home.mp3', 'artist': 'Michael Bublé', 'cover': 'n/a'},
-    {'id': 4, 'name': 'How Will I Know','link': 'songs/how_will_i_know.mp3', 'artist': 'Whitney Houston', 'cover': 'Sam Smith'},
-    {'id': 5, 'name': 'How You Gonna Act Like That','link': 'songs/how_you_gonna_act.mp3', 'artist': 'Tyrese', 'cover': 'n/a'},
-    {'id': 6, 'name': 'I Wanna Know', 'link': 'songs/i_wanna_know.mp3', 'artist': 'Joe', 'cover': 'n/a'},
-    {'id': 7, 'name': 'Make You Feel My Love', 'link': 'songs/make_you_feel_my_love.mp3', 'artist': 'Adele', 'cover': 'n/a'},
-    {'id': 8, 'name': 'Photograph', 'link': 'songs/photograph.mp3', 'artist': 'Ed Sheeran', 'cover': 'Brooklyn Duo'},
-    {'id': 9, 'name': 'Reason', 'link': 'songs/reason.mp3', 'artist': 'Calum Scott', 'cover': 'Leona Lewis'},
-    {'id': 10, 'name': 'Too Good at Goodbyes', 'link': 'songs/too_good.mp3', 'artist': 'Sam Smith', 'cover': 'n/a'}
+    {'id': 0, 'name': 'All of Me', 'link': 'all_of_me.mp3', 'artist': 'John Legend', 'cover': 'Lindsey Stirling'},
+    {'id': 1, 'name': 'Brown Eyes', 'link': 'brown_eyes.mp3', 'artist': 'Destiny\'s Child', 'cover': 'n/a'},
+    {'id': 2, 'name': 'Burn', 'link': 'burn.mp3', 'artist': 'Usher', 'cover': 'n/a'},
+    {'id': 3, 'name': 'Home', 'link': 'home.mp3', 'artist': 'Michael Bublé', 'cover': 'n/a'},
+    {'id': 4, 'name': 'How Will I Know','link': 'how_will_i_know.mp3', 'artist': 'Whitney Houston', 'cover': 'Sam Smith'},
+    {'id': 5, 'name': 'How You Gonna Act Like That','link': 'how_you_gonna_act.mp3', 'artist': 'Tyrese', 'cover': 'n/a'},
+    {'id': 6, 'name': 'I Wanna Know', 'link': 'i_wanna_know.mp3', 'artist': 'Joe', 'cover': 'n/a'},
+    {'id': 7, 'name': 'Make You Feel My Love', 'link': 'make_you_feel_my_love.mp3', 'artist': 'Adele', 'cover': 'n/a'},
+    {'id': 8, 'name': 'Photograph', 'link': 'photograph.mp3', 'artist': 'Ed Sheeran', 'cover': 'Brooklyn Duo'},
+    {'id': 9, 'name': 'Reason', 'link': 'reason.mp3', 'artist': 'Calum Scott', 'cover': 'Leona Lewis'},
+    {'id': 10, 'name': 'Too Good at Goodbyes', 'link': 'too_good.mp3', 'artist': 'Sam Smith', 'cover': 'n/a'}
     ]
+
+  for song in data_list:
+    original = song['link']
+    updated = 'static/songs/' + original
+    song['link'] = updated
+
   return data_list
 
 # Initialize Flask.
